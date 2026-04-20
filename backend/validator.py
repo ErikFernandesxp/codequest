eeea7664ed1a3517610602578df1be96b6cfe83f
ErikFernandesxp <gamermito222@gmail.com>
2026-04-20 17:40:36 -0300

    Update validator.py

--- backend/validator.py
+++ backend/validator.py
@@ -1,5 +1,11 @@
 def validar_codigo(resposta_usuario, resposta_correta):
     if resposta_usuario.strip() == resposta_correta.strip():
-        return True, "Perfeito! Você acertou 🎯"
-    else:
-        return False, f"Erro! O correto seria:\n{resposta_correta}"
+        return True, "Perfeito! Você entendeu 🎯"
+
+    if "printf" not in resposta_usuario:
+        return False, "Você esqueceu de usar printf!"
+
+    if ";" not in resposta_usuario:
+        return False, "Em C, não esqueça do ponto e vírgula!"
+
+    return False, "Quase! Revise a sintaxe e tente novamente."
