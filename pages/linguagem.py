a1502ee8627053d1ca735300db1f10ceea35497d
ErikFernandesxp <gamermito222@gmail.com>
2026-04-22 08:45:07 -0300

    Revise language selection options and UI elements
    Updated language options and button text for better clarity.

--- pages/linguagem.py
+++ pages/linguagem.py
@@ -3,21 +3,15 @@ import streamlit as st
 st.title("📚 Escolha sua Linguagem")
 
 opcoes = {
-    "Python": "python",
-    "C": "c",
-    "Java": "java",
-    "PHP": "php"
+    "Python 🐍": "python",
+    "C ⚙️": "c",
+    "Java ☕": "java",
+    "PHP 🌐": "php"
 }
 
-linguagem_nome = st.selectbox(
-    "Selecione a linguagem que deseja aprender:",
-    list(opcoes.keys())
-)
+ling = st.selectbox("Selecione:", list(opcoes.keys()))
 
-if st.button("Confirmar"):
-    # salva SEMPRE em minúsculo
-    st.session_state["linguagem"] = opcoes[linguagem_nome].lower()
+if st.button("🚀 Começar"):
+    st.session_state["linguagem"] = opcoes[ling]
     st.session_state["fase"] = 0
-
-    st.success(f"Você escolheu {linguagem_nome} 🚀")
     st.switch_page("fase")
