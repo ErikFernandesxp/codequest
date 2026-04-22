e97fa073d31142f031ee7236f2abf072c50252c1
ErikFernandesxp <gamermito222@gmail.com>
2026-04-22 09:49:58 -0300

    Update linguagem.py

--- pages/linguagem.py
+++ pages/linguagem.py
@@ -6,9 +6,8 @@ st.set_page_config(layout="centered")
 init_session(st)
 
 st.title("🎮 CodeQuest")
-st.subheader("📚 Escolha sua Linguagem")
+st.subheader("Escolha sua linguagem")
 
-# 🎯 Mapeamento correto
 opcoes = {
     "Python 🐍": "python",
     "C ⚙️": "c",
@@ -16,12 +15,10 @@ opcoes = {
     "PHP 🌐": "php"
 }
 
-# 👇 aqui o usuário vê bonito, mas o sistema usa o valor certo
 ling_label = st.selectbox("Selecione:", list(opcoes.keys()))
 
-# 🎮 botão iniciar
-if st.button("🚀 Começar"):
-    linguagem = opcoes[ling_label]  # ✅ pega valor correto
+if st.button("Começar"):
+    linguagem = opcoes[ling_label]
 
     st.session_state["linguagem"] = linguagem
     st.session_state["fase"] = 0
@@ -29,7 +26,5 @@ if st.button("🚀 Começar"):
     st.session_state["xp"] = 0
     st.session_state["nivel"] = 1
     st.session_state["vidas"] = 3
-
-    st.success(f"Você escolheu {ling_label} 🚀")
 
     st.switch_page("pages/fase.py")
